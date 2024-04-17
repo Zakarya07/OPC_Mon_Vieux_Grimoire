aa7a40f75e50afa904671cb50af7f484d3b1ea6f
Zakarya <zakarya.elkhattabi@gmail.com>
2024-04-17 19:29:26 +0200

    User Authentification: Login Part

--- controller/user.js
+++ controller/user.js
@@ -11,11 +11,33 @@ exports.signup = (req, res) => {
         password: hash,
       });
 
-      user.save()
-        .then( () => res.status(201).json({message: "Utilisateur creé !"}))
+      user
+        .save()
+        .then(() => res.status(201).json({ message: "Utilisateur créer !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 };
 
-exports.login = (req, res) => {};
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (user === null) {
+        res.status(401).json({ message: "Paire identifiant/mot de passe incorrecte." });
+      } else {
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            res.status(401).json({ message: "Paire identifiant/mot de passe incorrecte." })
+          } else {
+            res.status(200).json({
+              userId: user._id,
+              token: "TOKEN"
+            })
+          }
+        })
+        .catch(error => res.status(500).json({ error }));
+      }
+    })
+    .catch((error) => res.status(500).json({ error }));
+};
